5ee56358e08afe378cece954b5a84ac1f992c885
dying-surfer <ralph.schneller@gmail.com>
2024-03-22 18:55:06 +0100

    erste fakten
    Signed-off-by: dying-surfer <ralph.schneller@gmail.com>

--- gui/js/renderer.js
+++ gui/js/renderer.js
@@ -15,21 +15,55 @@ function handleFileChanged() {
 async function dataReady() {
     let hist = JSON.parse(await backend.loadMusicHistory());
 
+    add('<h2>Allgemein</h2>');
+
     addFact('Gehörte Lieder', hist.length);
+
+    const groupedByArtist = hist.reduce((acc, curr) => {
+        if (!acc[curr.artistName]) {
+            acc[curr.artistName] = 0;
+        }
+        acc[curr.artistName] += 1;
+        return acc;
+    }, {});
+
+    const sortedArtists = Object.entries(groupedByArtist)
+        .sort((a, b) => b[1] - a[1]);
+
+    console.log(sortedArtists);
+
+    addFact('Gehörte Künstler', sortedArtists.length);
+
+    addTop(30, 'Künstler', sortedArtists);
+
 }
 
 
-function debug(x){
-   console.log(x);
-   document.getElementById('debug').innerHTML += '<br>' + x;
+function debug(x) {
+    console.log(x);
+    document.getElementById('debug').innerHTML += '<br>' + x;
 }
 
-function addFact(label, value){
-    document.getElementById('content').innerHTML += `
+
+function addTop(n, label, map){
+    add(`<h2>Deine Top ${n} ${label} </h2>`);
+    let count = 1;
+    map.slice(0, n).forEach(([key, val]) => {
+        add(`${count}. ${key} ${val} <br>`);
+        count++;
+    });
+}
+
+function addFact(label, value) {
+    add(`
     <span class="fact">
         <span class="label">${label}</span>
         <span class="value">${value}</span>
     </span>
-`;
+    `);
 
+}
+
+function add(html) {
+    document.getElementById('content').innerHTML += html;
 }
