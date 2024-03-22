d670f2d75a849ea7d92795c1c2d44feefb827232
dying-surfer <ralph.schneller@gmail.com>
2024-03-22 17:55:07 +0100

    remove before initializing with new file
    Signed-off-by: dying-surfer <ralph.schneller@gmail.com>

--- src/api.js
+++ src/api.js
@@ -1,11 +1,13 @@
 const AdmZip = require("adm-zip");
 const path = require("path")
 const {app} = require('electron');
+const fs = require('fs')
 
 async function init(filepath) {
     const zip = new AdmZip(filepath);
 
     const outputDir = path.join(app.getPath('appData'), 'chartify', 'spotify');
+    fs.rmSync(outputDir, { recursive: true, force: true });
     zip.extractAllTo(outputDir);
     console.log(`Extracted all to ${outputDir}`);
     return 'ok';
