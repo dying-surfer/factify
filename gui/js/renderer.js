78bf005550aec0b410f9ef9bb5a3dd3e1f008d5d
dying-surfer <ralph.schneller@gmail.com>
2024-03-22 15:54:39 +0100

    exposing api
    Signed-off-by: dying-surfer <ralph.schneller@gmail.com>

--- gui/js/renderer.js
+++ gui/js/renderer.js
@@ -1,3 +1,10 @@
 window.addEventListener('DOMContentLoaded', () => {
-    document.querySelector('#node-version').innerText = versions.node;
+    backend.init('wooot')
+    .then((data) => {
+        dataReady();
+    })
 })
+
+function dataReady(){
+    document.getElementById('debug').innerText = 'Okay, kann los gehen';
+}
--- preload.js
+++ preload.js
@@ -1,7 +1,17 @@
-const { contextBridge } = require('electron')
+const { contextBridge, ipcRenderer} = require('electron')
 
 contextBridge.exposeInMainWorld('versions', {
   node: process.versions.node,
   chrome: process.versions.chrome,
   electron:  process.versions.electron
 })
+
+
+contextBridge.exposeInMainWorld('backend', {
+  'init': init
+})
+
+
+async function init(filePath){
+  return 'ok'
+}
